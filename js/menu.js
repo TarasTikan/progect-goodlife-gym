c20354702df478297a54407efbe90d4bfebc64fd
TarasTikan <tikantaras1@gmail.com>
2024-06-26 17:23:37 +0300

    add no-scroll

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,21 @@
-const btnOpenMenu = document.querySelector(".menu-open-btn");
+
+
+
+(() => {const btnOpenMenu = document.querySelector(".menu-open-btn");
 const btnClouseMenu = document.querySelector(".menu-close-btn");
 const mobMenu = document.querySelector(".mob-menu");
+const body = document.querySelector("body");
 
 btnOpenMenu.addEventListener("click", toggleMenu);
 btnClouseMenu.addEventListener("click", toggleMenu);
-
+mobMenu.addEventListener('click', removeMenu)
 function toggleMenu() {
   mobMenu.classList.toggle("visually-hidden");
+  body.classList.toggle("no-scroll");
 }
+
+function removeMenu() {
+  mobMenu.classList.add("visually-hidden");
+  body.classList.add("no-scroll");
+}
+})()
