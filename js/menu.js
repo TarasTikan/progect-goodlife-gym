f1f8e713b730bbd7747466de8c34f3c33ac8b4e9
TarasTikan <tikantaras1@gmail.com>
2024-06-27 14:01:26 +0300

    reset code

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
-(() => {const btnOpenMenu = document.querySelector(".menu-open-btn");
-const btnClouseMenu = document.querySelector(".menu-close-btn");
-const mobMenu = document.querySelector(".mob-menu");
-const body = document.querySelector("body");
+(() => {
+  const btnOpenMenu = document.querySelector(".menu-open-btn");
+  const btnClouseMenu = document.querySelector(".menu-close-btn");
+  const mobMenu = document.querySelector(".mob-menu");
+  const body = document.querySelector("body");
 
-btnOpenMenu.addEventListener("click", toggleMenu);
-btnClouseMenu.addEventListener("click", toggleMenu);
+  btnOpenMenu.addEventListener("click", toggleMenu);
+  btnClouseMenu.addEventListener("click", toggleMenu);
 
-function toggleMenu() {
-  mobMenu.classList.toggle("visually-hidden");
-  body.classList.toggle("no-scroll");
-}
-
-})()
+  function toggleMenu() {
+    mobMenu.classList.toggle("visually-hidden");
+    body.classList.toggle("no-scroll");
+  }
+})();
